66b91caff964cf3292217bf6677df0e0dc586f47
一去二三里 <572906809@qq.com>
2019-08-30 17:15:40 +0800

    dddddd

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
 
-!/usr/bin/env sh
-
-# localhost 8080
-# npm run dev
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
-echo $1
-
 # 生成静态文件
-npm run build $1
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -20,21 +15,13 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-# npm run d 'commit值'
-git commit -m $1
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
 git push -f git@github.com:OneGoesTwoThereMiles/Studying.git master:gh-pages
 
 cd -
-
-git add .
-
-git commit -m $1
-
-git push origin master:master # 推到github上
